b096430602cbfa0a25d58ae0e8b97f737e43ccd8
Luong Thanh Tung <tunglt@tunglt.itech.com.vn>
2019-11-28 10:27:31 +0700

    add dist folder

---
+++ dist/serviceWorker.js
@@ -0,0 +1,36 @@
+importScripts('workbox-sw.prod.v2.1.2.js');
+
+const workboxSW = new self.WorkboxSW({clientsClaim: true})
+workboxSW.precache([
+  {
+    "url": "favicon-32x32.png",
+    "revision": "7b1da026f1c9dc9d8caadbe4bdb7b2a8"
+  },
+  {
+    "url": "index.html",
+    "revision": "5374eab0cd4f0d587e0c69ba924836df"
+  },
+  {
+    "url": "itkVtkViewer.js",
+    "revision": "2d1ff57963eb6b209f0f53e5f1541f79"
+  },
+  {
+    "url": "test.html",
+    "revision": "7ae13ed21ff30dda487760df4b24897c"
+  },
+  {
+    "url": "workbox-sw.prod.v2.1.3.js",
+    "revision": "a9890beda9e5f17e4c68f42324217941"
+  }
+])
+
+workboxSW.router.registerRoute(
+  /\.js|\.png|\.wasm$/,
+  workboxSW.strategies.staleWhileRevalidate({
+  cacheName: 'staleWhileRevalidateContent',
+  cacheExpiration: {
+    maxEntries: 50,
+    maxAgeSeconds: 7 * 24 * 60 * 60 * 26,
+    }
+  })
+);
